26f675aa499fe01bb2b49df507247b43c50f01ab
David van Leusen <jvleusen@gmail.com>
2014-04-22 19:37:40 +0200

    Project Base

---
+++ src/main/java/nl/rug/jbi/jsm/Bootstrap.java
@@ -0,0 +1,7 @@
+package nl.rug.jbi.jsm;
+
+public class Bootstrap {
+    public static void main(final String[] args) {
+        throw new UnsupportedOperationException("Not Yet Implemented");
+    }
+}
