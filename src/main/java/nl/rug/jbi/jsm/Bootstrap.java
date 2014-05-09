9f346077da54de376693fcba856eccd614f9dfc5
David van Leusen <jvleusen@gmail.com>
2014-05-09 20:53:57 +0200

    Add dependencies for Logging and Java Library Funcs

--- src/main/java/nl/rug/jbi/jsm/Bootstrap.java
+++ src/main/java/nl/rug/jbi/jsm/Bootstrap.java
@@ -1,7 +1,13 @@
 package nl.rug.jbi.jsm;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Bootstrap {
+    private static final Logger logger = LogManager.getLogger(Bootstrap.class);
+
     public static void main(final String[] args) {
-        throw new UnsupportedOperationException("Not Yet Implemented");
+        logger.trace("Starting Application");
+        logger.trace("Exiting Application");
     }
 }
